55a108b881f31283f752a696692c12ffb40743fa
Vani1B <vani.boddu@gmail.com>
2025-05-01 14:56:02 +0530

    changes made in install.sh and start.sh for error handling

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/backend
-npm install
+cd /home/ec2-user/backend || exit 1
+echo "Running npm install..."
+npm install >> /home/ec2-user/backend/install.log 2>&1
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/backend
+cd /home/ec2-user/backend || exit 1
+echo "Starting Node server..."
 node server.js > app.log 2>&1 &
